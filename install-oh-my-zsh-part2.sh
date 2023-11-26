340f3051237d47fdd1f8e5ad0ab433ec7c57147d
Eliran Ben Maor <eliran.benmaor@infinitylabs.co.il>
2023-11-26 07:07:20 +0200

    Fixed script commands and added auto donwload font

--- install-oh-my-zsh-part2.sh
+++ install-oh-my-zsh-part2.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
+#Download the Font:
+wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+
+mkdir -p ~/.local/share/fonts
+mv MesloLGS_NF_Regular.ttf ~/.local/share/fonts/
+fc-cache -f -v
